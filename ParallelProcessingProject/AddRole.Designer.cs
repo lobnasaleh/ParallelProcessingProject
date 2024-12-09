9f52e591368660a78a287bc196223b86dc2e0c81
lobnaSaleh <lobna.saleh2003@gmail.com>
2024-12-09 15:22:09 +0200

    AddRole

---
+++ ParallelProcessingProject/AddRole.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ParallelProcessingProject
+{
+    partial class AddRole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label5 = new Label();
+            label4 = new Label();
+            rolename = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            button4 = new Button();
+            rolee = new Label();
+            SuspendLayout();
+            // 
+            // label5
+            // 
+            label5.BackColor = SystemColors.Highlight;
+            label5.Font = new Font("Microsoft New Tai Lue", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(-8, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(510, 78);
+            label5.TabIndex = 7;
+            label5.Text = "ATM Management System";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Tai Le", 13.8F, FontStyle.Bold);
+            label4.ForeColor = SystemColors.Highlight;
+            label4.Location = new Point(390, 78);
+            label4.Name = "label4";
+            label4.Size = new Size(85, 29);
+            label4.TabIndex = 8;
+            label4.Text = "Admin";
+            // 
+            // rolename
+            // 
+            rolename.Location = new Point(228, 223);
+            rolename.Multiline = true;
+            rolename.Name = "rolename";
+            rolename.Size = new Size(247, 40);
+            rolename.TabIndex = 9;
+            rolename.TextChanged += username_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Tai Le", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Highlight;
+            label2.Location = new Point(53, 234);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 29);
+            label2.TabIndex = 10;
+            label2.Text = "Role Name";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Tai Le", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.GrayText;
+            label1.Location = new Point(163, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 29);
+            label1.TabIndex = 11;
+            label1.Text = "Add New Role";
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.GradientActiveCaption;
+            button4.Font = new Font("Microsoft YaHei UI", 13.8F, FontStyle.Bold);
+            button4.ForeColor = SystemColors.Highlight;
+            button4.Location = new Point(151, 348);
+            button4.Name = "button4";
+            button4.Size = new Size(177, 45);
+            button4.TabIndex = 12;
+            button4.Text = "Add";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // rolee
+            // 
+            rolee.AutoSize = true;
+            rolee.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rolee.ForeColor = Color.Red;
+            rolee.Location = new Point(228, 266);
+            rolee.Name = "rolee";
+            rolee.Size = new Size(0, 23);
+            rolee.TabIndex = 13;
+            // 
+            // AddRole
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(499, 450);
+            Controls.Add(rolee);
+            Controls.Add(button4);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(rolename);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "AddRole";
+            Text = "AddRole";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label5;
+        private Label label4;
+        private TextBox rolename;
+        private Label label2;
+        private Label label1;
+        private Button button4;
+        private Label rolee;
+    }
+}
---
+++ ParallelProcessingProject/AddRole.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace ParallelProcessingProject
+{
+    public partial class AddRole : Form
+    {
+        public AddRole()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=ATM;Integrated Security=True;TrustServerCertificate=True");
+        private bool checkDuplicateUserName(string userName)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmdd = new SqlCommand("checkDuplicateRoleName", conn);//esm el procedure
+                cmdd.CommandType = CommandType.StoredProcedure;
+                cmdd.Parameters.AddWithValue("@rolename", rolename.Text);
+
+                int count = (int)cmdd.ExecuteScalar();//returning a single value zy el count wa el min wa el max
+                return count > 0;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+                return true;
+
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+
+            }
+        }
+        private void username_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(rolename.Text))
+            {
+                // usernameerr.Visible = true;
+
+                rolee.Text = "Please enter a Role Name";
+            }
+            else
+            {
+                try
+                {
+                    if (checkDuplicateUserName(rolename.Text))
+                    {
+
+                        rolee.Text = "This Role already exists";
+
+                    }
+                    else
+                    {
+                        rolee.Visible = false;
+                        try
+                        {
+                            conn.Open();
+
+                            //@username nvarchar(50),@password nvarchar(255),@balance decimal (18,2),@role int
+                            SqlCommand cmd = new SqlCommand("addrole", conn);//esm el procedure
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@rolename", rolename.Text);
+                          
+                            int rowsAffected = cmd.ExecuteNonQuery();//te3ml execute lel query w aterga3 el rows ely affected
+
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Role added successfully.");
+                            }
+                            else
+                            {
+                                MessageBox.Show("No rows affected. The Role was not added.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                            MessageBox.Show(ex.ToString());
+
+
+                        }
+                        finally
+                        {
+                            conn.Close();
+
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+            }
+           
+          
+                
+            }
+            }
+    }
+
